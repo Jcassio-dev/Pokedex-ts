35c2ad91f1e89f18cf7c5c273a7198b3dbe85f4f
José Cássio <josecassio2013@gmail.com>
2023-03-26 22:29:42 -0300

    add: react-query & refactor api fetch

--- src/pages/pokedex/Pokedex.tsx
+++ src/pages/pokedex/Pokedex.tsx
@@ -3,14 +3,14 @@ import { useState, useEffect } from 'react';
 
 
 
-import { listPokemons, PokemonListInterface } from '../../services/listPokemons';
+import { listPokemons } from '../../services/listPokemons';
 
 import { AppBarButton } from '../../components/AppBar';
 import { PokeDexCard } from '../../components/PokeDexCard';
 
 import Container from '@mui/material/Container';
 import {Box, Grid} from "@mui/material"
-
+import { useQuery } from 'react-query'
 
 
 
@@ -21,24 +21,17 @@ interface PokedexProps {
 
 
 export const Pokedex: React.FC<PokedexProps> = () => {
-    const [pokemons, setPokemons] = useState<Pokemo[]>([]);
+    //const [pokemons, setPokemons] = useState<Pokemo[]>([]);
 
-   
-
-    useEffect(()=> {
-        listPokemons()
-        .then(response => setPokemons(response.results))
-
-    }, [])
-
-
+    const { data } = useQuery(`listPokemons`, listPokemons);
+    
     return (
         <div>
             <AppBarButton name="pokedex"/>
             <Container maxWidth="lg">
             <Box mt={3}>
                 <Grid container spacing={2}>
-            {pokemons && pokemons.map((pokemon, index) => (
+            {data && data?.results.map((pokemon, index) => (
                 <Grid item xs={6} lg={3} key={index}> 
                     <PokeDexCard pokemon={pokemon}/>
                 </Grid>
